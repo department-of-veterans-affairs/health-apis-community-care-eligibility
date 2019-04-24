87e28882b44699eb14bce78dafb76eb7e35f62b2
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-04-24 11:11:10 -0400

    Merge clean up.

--- community-care-eligibility/src/main/java/gov/va/api/health/communitycareeligibility/service/FacilityClient.java
+++
@@ -1,7 +0,0 @@
-package gov.va.api.health.communitycareeligibility.service;
-
-import gov.va.api.health.communitycareeligibility.api.CommunityCareEligibilityResponse.Coordinates;
-
-public interface FacilityClient {
-  VaFacilitiesResponse facilities(Coordinates patientAddress, String serviceType);
-}
