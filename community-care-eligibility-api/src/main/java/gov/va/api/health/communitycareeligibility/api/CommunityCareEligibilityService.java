c0836a0f9a493b303c1b71735f23032045dc7c32
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-05-29 17:18:04 -0400

    Swagger

--- community-care-eligibility-api/src/main/java/gov/va/api/health/communitycareeligibility/api/CommunityCareEligibilityService.java
+++ community-care-eligibility-api/src/main/java/gov/va/api/health/communitycareeligibility/api/CommunityCareEligibilityService.java
@@ -17,36 +17,14 @@ import javax.ws.rs.Path;
       @Info(
         title = "Community Care Eligibility",
         version = "v0",
-        description = "Community care eligibility under the MISSION Act."
-        // FHIR (Fast Healthcare Interoperability Resources) specification defines a
-        // set of "Resources" that represent granular clinical, financial, and administrative
-        // concepts.  This CoverageEligibilityResponse resource is compliant with FHIR version R4
-        // standards.
-
-        // The Community Care Eligibility API utilizes the Facility API to determine Veteran wait
-        // and drive time, and computes a Veteran's community care eligibility status under the
-        // criteria of the MISSION Act. This API provides access via an EHR-agnostic RESTful web
-        // services abstraction layer.
-
-        //        This API is a [Spring Boot](https://spring.io/projects/spring-boot) microservice
-        //        	that computes *objective* overall community-care eligibility by combining
-        // eligibility codes
-        //        	from the Eligibility and Enrollment System (E&E) with wait- and drive-time access
-        //        	standards.
-        //        	(Average historical wait times are provided by Facilities API. Average drive
-        // times are computed by Bing Maps. )
-        //
-        //        	Bing Maps is expected to be removed in the near future, when average drive times
-        // are supplied
-        //        	by the Facilities API. Historical wait times from the Facilities API are expected
-        // to be replaced
-        //        	with live wait times from the VA Online Scheduling System.
+        description =
+            "This API computes *objective* overall community-care eligibility by combining eligibility codes from the Eligibility and Enrollment System (E&E)"
+                + " with wait- and drive-time access standards from the Facilities API. "
       ),
   externalDocs =
       @ExternalDocumentation(
         description = "GitHub",
-        url =
-            "https://github.com/department-of-veterans-affairs/health-apis-community-care-eligibility"
+        url = "https://github.com/department-of-veterans-affairs/health-apis-community-care-eligibility"
       )
 )
 @Path("/")
