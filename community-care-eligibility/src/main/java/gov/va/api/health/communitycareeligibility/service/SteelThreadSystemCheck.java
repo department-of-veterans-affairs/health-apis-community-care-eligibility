1f6bf55758c59cf6d991954c2f5da862d471be49
dbrown-va <43859081+dbrown-va@users.noreply.github.com>
2019-06-11 16:47:39 -0400

    CCE-71 Steel thread health check (#33)

---
+++ community-care-eligibility/src/main/java/gov/va/api/health/communitycareeligibility/service/SteelThreadSystemCheck.java
@@ -0,0 +1,72 @@
+package gov.va.api.health.communitycareeligibility.service;
+
+import gov.va.api.health.communitycareeligibility.api.CommunityCareEligibilityResponse.Address;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+
+@Component
+@Slf4j
+public class SteelThreadSystemCheck implements HealthIndicator {
+
+  private final EligibilityAndEnrollmentClient eeClient;
+
+  private final FacilitiesClient facilitiesClient;
+
+  private final String icn;
+
+  private final Address address;
+
+  private final int driveMinutes;
+
+  /** 'By hand' all args constructor is required to inject non-string values from our properties. */
+  public SteelThreadSystemCheck(
+      @Autowired EligibilityAndEnrollmentClient eeClient,
+      @Autowired FacilitiesClient facilitiesClient,
+      @Value("${health-check.icn}") String icn) {
+    this.eeClient = eeClient;
+    this.facilitiesClient = facilitiesClient;
+    this.icn = icn;
+    this.address =
+        Address.builder()
+            .city("Melbourne")
+            .state("FL")
+            .zip("32934")
+            .street("505 N John Rodes Blvd")
+            .build();
+    this.driveMinutes = 60;
+  }
+
+  @Override
+  @SneakyThrows
+  public Health health() {
+    if ("skip".equals(icn)) {
+      return Health.up().withDetail("skipped", true).build();
+    }
+    try {
+      eeClient.requestEligibility(icn);
+      facilitiesClient.nearby(address, driveMinutes);
+      return Health.up().build();
+    } catch (HttpServerErrorException
+        | HttpClientErrorException
+        | ResourceAccessException
+        | Exceptions.EeUnavailableException
+        | Exceptions.FacilitiesUnavailableException e) {
+
+      return Health.down()
+          .withDetail("exception", e.getClass())
+          .withDetail("message", e.getMessage())
+          .build();
+    } catch (Exception e) {
+      log.error("Failed to complete health check.", e);
+      throw e;
+    }
+  }
+}
---
+++ community-care-eligibility/src/test/java/gov/va/api/health/communitycareeligibility/service/SteelThreadSystemCheckTest.java
@@ -0,0 +1,59 @@
+package gov.va.api.health.communitycareeligibility.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import gov.va.med.esr.webservices.jaxws.schemas.GetEESummaryResponse;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.actuate.health.Status;
+
+public class SteelThreadSystemCheckTest {
+
+  @Mock EligibilityAndEnrollmentClient eeClient;
+  @Mock FacilitiesClient facilitiesClient;
+
+  @Before
+  public void _init() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void healthCheckHappyPathEe() {
+    GetEESummaryResponse root = new GetEESummaryResponse();
+    SteelThreadSystemCheck test = new SteelThreadSystemCheck(eeClient, facilitiesClient, "123");
+    when(eeClient.requestEligibility(Mockito.any())).thenReturn(root);
+    assertThat(test.health().getStatus()).isEqualTo(Status.UP);
+  }
+
+  @Test
+  public void healthCheckHappyPathFacilities() {
+    List<String> facilities = new ArrayList<>();
+    SteelThreadSystemCheck test = new SteelThreadSystemCheck(eeClient, facilitiesClient, "123");
+    when(facilitiesClient.nearby(Mockito.any(), Mockito.anyInt())).thenReturn(facilities);
+    assertThat(test.health().getStatus()).isEqualTo(Status.UP);
+  }
+
+  @Test
+  public void healthCheckSadPathEe() {
+
+    SteelThreadSystemCheck test = new SteelThreadSystemCheck(eeClient, facilitiesClient, "123");
+    when(eeClient.requestEligibility(Mockito.any()))
+        .thenThrow(new Exceptions.EeUnavailableException(new Throwable()));
+    assertThat(test.health().getStatus()).isEqualTo(Status.DOWN);
+  }
+
+  @Test
+  public void healthCheckSadPathFacilities() {
+
+    SteelThreadSystemCheck test = new SteelThreadSystemCheck(eeClient, facilitiesClient, "123");
+    when(facilitiesClient.nearby(Mockito.any(), Mockito.anyInt()))
+        .thenThrow(new Exceptions.FacilitiesUnavailableException(new Throwable()));
+    assertThat(test.health().getStatus()).isEqualTo(Status.DOWN);
+  }
+}
