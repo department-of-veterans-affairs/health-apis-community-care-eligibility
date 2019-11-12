4daa8b33ad3f160295379e8574c84327445a3f24
jnguy-va <jennifer.nguy@libertyits.com>
2019-11-12 14:05:35 -0500

    Token endpoint

--- community-care-eligibility-api/src/main/java/gov/va/api/health/communitycareeligibility/api/CommunityCareEligibilityService.java
+++ community-care-eligibility-api/src/main/java/gov/va/api/health/communitycareeligibility/api/CommunityCareEligibilityService.java
@@ -57,7 +57,7 @@ import javax.ws.rs.Path;
         implicit =
             @OAuthFlow(
               authorizationUrl = "https://dev-api.va.gov/oauth2/authorization",
-              tokenUrl = "https://dev-api.va.gov/services/fhir/v0/dstu2/token",
+              tokenUrl = "https://dev-api.va.gov/oauth2/token",
               scopes = {
                 @OAuthScope(name = "patient/CommunityCareEligibility.read", description = "Community Care Eligibility")
               }
