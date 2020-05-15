20e0d693fc7427ee8c37e73e7abccad7456d902a
jhulbert <joshua.hulbert@libertyits.com>
2020-05-15 17:26:49 -0400

    Home Controller only using Json

--- community-care-eligibility/src/main/java/gov/va/api/health/communitycareeligibility/service/HomeControllerV0.java
+++ community-care-eligibility/src/main/java/gov/va/api/health/communitycareeligibility/service/HomeControllerV0.java
@@ -1,11 +1,12 @@
 package gov.va.api.health.communitycareeligibility.service;
 
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import gov.va.api.health.autoconfig.configuration.JacksonConfig;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,31 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/v0/eligibility", produces = "application/json")
 public class HomeControllerV0 {
-  private static final YAMLMapper MAPPER = new YAMLMapper();
+
+  private final Resource openapi;
+
+  @Autowired
+  HomeControllerV0(@Value("classpath:/openapi.json") Resource openapi) {
+    this.openapi = openapi;
+  }
 
   /** REST endpoint for OpenAPI JSON + redirect. */
   @GetMapping(
-      value = {"/", "/openapi.json", "/api/openapi.json"},
+      value = {"/", "/openapi.json"},
       produces = "application/json")
   @ResponseBody
   public Object openApiJson() throws IOException {
-    return MAPPER.readValue(openApiYamlContent(), Object.class);
-  }
-
-  /** REST endpoint OpenAPI YAML. */
-  @GetMapping(
-      value = {"/openapi.yaml", "/api/openapi.yaml"},
-      produces = "application/vnd.oai.openapi")
-  @ResponseBody
-  public String openApiYaml() throws IOException {
-    return openApiYamlContent();
-  }
-
-  /** OpenAPI content in YAML form. */
-  @Bean
-  private String openApiYamlContent() throws IOException {
-    try (InputStream is = new ClassPathResource("openapi.yaml").getInputStream()) {
-      return StreamUtils.copyToString(is, Charset.defaultCharset());
+    try (InputStream is = openapi.getInputStream()) {
+      String openapiContent = StreamUtils.copyToString(is, Charset.defaultCharset());
+      return JacksonConfig.createMapper().readValue(openapiContent, Object.class);
     }
   }
 }
