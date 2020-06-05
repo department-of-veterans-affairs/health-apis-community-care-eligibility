f81716fcff187c9cdb7e6aed2d909550da25a03c
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2020-06-05 08:52:44 -0400

    health check v0

--- community-care-eligibility/src/main/java/gov/va/api/health/communitycareeligibility/service/HealthController.java
+++ community-care-eligibility/src/main/java/gov/va/api/health/communitycareeligibility/service/HealthController.java
@@ -85,7 +85,7 @@ public class HealthController {
    * @see #clearCacheScheduled()
    */
   @Cacheable("health")
-  @GetMapping(value = "/health")
+  @GetMapping(value = "/v0/health")
   public ResponseEntity<Health> health() {
     return health(Instant.now());
   }
