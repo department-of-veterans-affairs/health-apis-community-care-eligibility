72337f63189568bc4d83b45bf2644efbee7ae393
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2020-06-23 16:41:03 -0400

    .

--- community-care-eligibility-tests/src/test/java/gov/va/api/health/communitycareeligibility/tests/OpenApiIT.java
+++ community-care-eligibility-tests/src/test/java/gov/va/api/health/communitycareeligibility/tests/OpenApiIT.java
@@ -1,11 +1,7 @@
 package gov.va.api.health.communitycareeligibility.tests;
 
-import static gov.va.api.health.communitycareeligibility.tests.SystemDefinitions.cceClient;
 import static gov.va.api.health.communitycareeligibility.tests.Requestor.makeRequest;
-import gov.va.api.health.sentinel.ExpectedResponse;
-import io.restassured.http.Method;
-import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
+
 import org.junit.jupiter.api.Test;
 
 public class OpenApiIT {
