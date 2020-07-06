10c8970191be03a2b9d89f6556c17e190885867f
eclendenning-va <evan.clendenning@libertyits.com>
2020-07-06 09:27:39 -0400

    more ITs with useful validations

--- community-care-eligibility-tests/src/test/java/gov/va/api/health/communitycareeligibility/tests/SearchIT.java
+++ community-care-eligibility-tests/src/test/java/gov/va/api/health/communitycareeligibility/tests/SearchIT.java
@@ -2,11 +2,23 @@ package gov.va.api.health.communitycareeligibility.tests;
 
 import static gov.va.api.health.communitycareeligibility.tests.Requestor.makeRequest;
 import static gov.va.api.health.communitycareeligibility.tests.SystemDefinitions.systemDefinition;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assumptions.assumeThat;
 
 import gov.va.api.health.communitycareeligibility.api.CommunityCareEligibilityResponse;
+import gov.va.api.health.sentinel.Environment;
+import java.util.Objects;
 import org.junit.jupiter.api.Test;
 
 public class SearchIT {
+  private void assumeStagingLabOrLab() {
+    assumeThat(Environment.get())
+        .overridingErrorMessage("Skipping in " + Environment.get())
+        .isNotEqualTo(Environment.LOCAL)
+        .isNotEqualTo(Environment.STAGING)
+        .isNotEqualTo(Environment.PROD);
+  }
+
   @Test
   void audiology() {
     String request =
@@ -95,6 +107,109 @@ public class SearchIT {
             "v0/eligibility/search?patient=%s&serviceType=%s",
             systemDefinition().patient(), "Orthopedics");
     makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void patientEligibleDueToCode() {
+    assumeStagingLabOrLab();
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s", "1013120787V412913", "PrimaryCare");
+    CommunityCareEligibilityResponse response =
+        makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+    assertThat(
+            response.eligibilityCodes().stream()
+                .filter(Objects::nonNull)
+                .anyMatch(c -> "N".equals(c.code())))
+        .isTrue();
+    assertThat(response.eligible()).isTrue();
+  }
+
+  @Test
+  void patientEligibleDueToHardship() {
+    assumeStagingLabOrLab();
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s", "1012845331V153043", "PrimaryCare");
+    CommunityCareEligibilityResponse response =
+        makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+    assertThat(response.eligible()).isTrue();
+  }
+
+  @Test
+  void patientEligibleDueToNoNearbyFacilities() {
+    assumeStagingLabOrLab();
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s", "1012845943V900681", "PrimaryCare");
+    CommunityCareEligibilityResponse response =
+        makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+    assertThat(response.nearbyFacilities()).isEmpty();
+    assertThat(response.eligible()).isTrue();
+  }
+
+  @Test
+  void patientEligibleDueToStateWithNoVaMedicalCenters() {
+    assumeStagingLabOrLab();
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s", "1012845944V882130", "PrimaryCare");
+    CommunityCareEligibilityResponse response =
+        makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+    assertThat(response.noFullServiceVaMedicalFacility()).isTrue();
+    assertThat(response.eligible()).isTrue();
+  }
+
+  @Test
+  void patientIndeterminateDueToNoAddress() {
+    assumeStagingLabOrLab();
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s", "1013117618V394810", "PrimaryCare");
+    CommunityCareEligibilityResponse response =
+        makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+    assertThat(response.patientAddress()).isNull();
+    assertThat(response.eligible()).isNull();
+  }
+
+  @Test
+  void patientIndeterminateDueToNoGeoCoding() {
+    assumeStagingLabOrLab();
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s", "1013120801V413425", "PrimaryCare");
+    CommunityCareEligibilityResponse response =
+        makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+    assertThat(response.patientCoordinates()).isNull();
+    assertThat(response.eligible()).isNull();
+  }
+
+  @Test
+  void patientNotEligibleDueToCode() {
+    assumeStagingLabOrLab();
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s", "1013120835V054496", "PrimaryCare");
+    CommunityCareEligibilityResponse response =
+        makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+    assertThat(
+            response.eligibilityCodes().stream()
+                .filter(Objects::nonNull)
+                .anyMatch(c -> "X".equals(c.code())))
+        .isTrue();
+    assertThat(response.eligible()).isFalse();
+  }
+
+  @Test
+  void patientNotEligibleDueToNearbyFacility() {
+    assumeStagingLabOrLab();
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s", "1012853802V084487", "PrimaryCare");
+    CommunityCareEligibilityResponse response =
+        makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+    assertThat(response.nearbyFacilities()).isNotEmpty();
+    assertThat(response.eligible()).isFalse();
   }
 
   @Test
