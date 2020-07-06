0d7644a90a5d74815df569493d23bc5d1f7a21d4
eclendenning-va <evan.clendenning@libertyits.com>
2020-07-06 11:05:33 -0400

    refactor into basic and advanced tests

--- community-care-eligibility-tests/src/test/java/gov/va/api/health/communitycareeligibility/tests/AdvancedSearchIT.java
+++ community-care-eligibility-tests/src/test/java/gov/va/api/health/communitycareeligibility/tests/AdvancedSearchIT.java
@@ -1,17 +1,18 @@
 package gov.va.api.health.communitycareeligibility.tests;
 
 import static gov.va.api.health.communitycareeligibility.tests.Requestor.makeRequest;
-import static gov.va.api.health.communitycareeligibility.tests.SystemDefinitions.systemDefinition;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assumptions.assumeThat;
 
 import gov.va.api.health.communitycareeligibility.api.CommunityCareEligibilityResponse;
 import gov.va.api.health.sentinel.Environment;
 import java.util.Objects;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class SearchIT {
-  private void assumeStagingLabOrLab() {
+public class AdvancedSearchIT {
+  @BeforeAll
+  public static void assumeStagingLabOrLab() {
     assumeThat(Environment.get())
         .overridingErrorMessage("Skipping in " + Environment.get())
         .isNotEqualTo(Environment.LOCAL)
@@ -20,98 +21,7 @@ public class SearchIT {
   }
 
   @Test
-  void audiology() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "Audiology");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
-  void cardiology() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "Cardiology");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
-  void dermatology() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "Dermatology");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
-  void gastroenterology() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "Gastroenterology");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
-  void gynecology() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "Gynecology");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
-  void mentalHealthCare() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "MentalHealthCare");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
-  void nutrition() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "Nutrition");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
-  void ophthalmology() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "Ophthalmology");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
-  void optometry() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "Optometry");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
-  void orthopedics() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "Orthopedics");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
   void patientEligibleDueToCode() {
-    assumeStagingLabOrLab();
     String request =
         String.format(
             "v0/eligibility/search?patient=%s&serviceType=%s", "1013120787V412913", "PrimaryCare");
@@ -127,7 +37,6 @@ public class SearchIT {
 
   @Test
   void patientEligibleDueToHardship() {
-    assumeStagingLabOrLab();
     String request =
         String.format(
             "v0/eligibility/search?patient=%s&serviceType=%s", "1012845331V153043", "PrimaryCare");
@@ -138,7 +47,6 @@ public class SearchIT {
 
   @Test
   void patientEligibleDueToNoNearbyFacilities() {
-    assumeStagingLabOrLab();
     String request =
         String.format(
             "v0/eligibility/search?patient=%s&serviceType=%s", "1012845943V900681", "PrimaryCare");
@@ -150,7 +58,6 @@ public class SearchIT {
 
   @Test
   void patientEligibleDueToStateWithNoVaMedicalCenters() {
-    assumeStagingLabOrLab();
     String request =
         String.format(
             "v0/eligibility/search?patient=%s&serviceType=%s", "1012845944V882130", "PrimaryCare");
@@ -162,7 +69,6 @@ public class SearchIT {
 
   @Test
   void patientIndeterminateDueToNoAddress() {
-    assumeStagingLabOrLab();
     String request =
         String.format(
             "v0/eligibility/search?patient=%s&serviceType=%s", "1013117618V394810", "PrimaryCare");
@@ -186,7 +92,6 @@ public class SearchIT {
 
   @Test
   void patientNotEligibleDueToCode() {
-    assumeStagingLabOrLab();
     String request =
         String.format(
             "v0/eligibility/search?patient=%s&serviceType=%s", "1013120835V054496", "PrimaryCare");
@@ -202,7 +107,6 @@ public class SearchIT {
 
   @Test
   void patientNotEligibleDueToNearbyFacility() {
-    assumeStagingLabOrLab();
     String request =
         String.format(
             "v0/eligibility/search?patient=%s&serviceType=%s", "1012853802V084487", "PrimaryCare");
@@ -210,41 +114,5 @@ public class SearchIT {
         makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
     assertThat(response.nearbyFacilities()).isNotEmpty();
     assertThat(response.eligible()).isFalse();
-  }
-
-  @Test
-  void podiatry() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "Podiatry");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
-  void primaryCare() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "PrimaryCare");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
-  void urology() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "Urology");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
-  }
-
-  @Test
-  void womensHealth() {
-    String request =
-        String.format(
-            "v0/eligibility/search?patient=%s&serviceType=%s",
-            systemDefinition().patient(), "WomensHealth");
-    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
   }
 }
---
+++ community-care-eligibility-tests/src/test/java/gov/va/api/health/communitycareeligibility/tests/BasicSearchIT.java
@@ -0,0 +1,136 @@
+package gov.va.api.health.communitycareeligibility.tests;
+
+import static gov.va.api.health.communitycareeligibility.tests.Requestor.makeRequest;
+import static gov.va.api.health.communitycareeligibility.tests.SystemDefinitions.systemDefinition;
+
+import gov.va.api.health.communitycareeligibility.api.CommunityCareEligibilityResponse;
+import org.junit.jupiter.api.Test;
+
+public class BasicSearchIT {
+
+  @Test
+  void audiology() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "Audiology");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void cardiology() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "Cardiology");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void dermatology() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "Dermatology");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void gastroenterology() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "Gastroenterology");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void gynecology() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "Gynecology");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void mentalHealthCare() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "MentalHealthCare");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void nutrition() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "Nutrition");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void ophthalmology() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "Ophthalmology");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void optometry() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "Optometry");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void orthopedics() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "Orthopedics");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void podiatry() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "Podiatry");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void primaryCare() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "PrimaryCare");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void urology() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "Urology");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+
+  @Test
+  void womensHealth() {
+    String request =
+        String.format(
+            "v0/eligibility/search?patient=%s&serviceType=%s",
+            systemDefinition().patient(), "WomensHealth");
+    makeRequest(request, 200).expectValid(CommunityCareEligibilityResponse.class);
+  }
+}
