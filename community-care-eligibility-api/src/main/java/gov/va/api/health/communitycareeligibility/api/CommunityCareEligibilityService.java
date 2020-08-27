a85f45af9510359e67be94732ac2c593a2b93ad8
taylorrassmannlibertyits <64798603+taylorrassmannlibertyits@users.noreply.github.com>
2020-08-27 16:35:07 -0400

    Updated CCE documentation to contain authorization info and page link. Also cleaned up some of the beginning text and added partial success scenarios.

--- community-care-eligibility-api/src/main/java/gov/va/api/health/communitycareeligibility/api/CommunityCareEligibilityService.java
+++ community-care-eligibility-api/src/main/java/gov/va/api/health/communitycareeligibility/api/CommunityCareEligibilityService.java
@@ -31,14 +31,34 @@ import javax.ws.rs.Path;
             title = "Community Care Eligibility",
             version = "v0",
             description =
-                "Compute community care eligibility under the **objective** criteria"
-                    + " of the MISSION Act. Because MISSION Act also includes"
+                "## Background"
+                    + "\n\n"
+                    + "Community care eligibility is computed under the **objective** criteria"
+                    + " of the MISSION Act. Since the MISSION Act also includes"
                     + " subjective criteria, this API does not provide a **final**"
                     + " eligibility decision. Any user-facing message based on these"
                     + " results should indicate that the patient is *probably* eligible"
                     + " or *probably not* eligible, and that no decision is final until"
-                    + " they have consulted VA staff and"
-                    + " scheduled their appointment."),
+                    + " they have consulted VA staff and scheduled their appointment."
+                    + "\n\n"
+                    + "## Authorization"
+                    + "\n\n"
+                    + "This API uses the [OpenID Connect](https://openid.net/specs/openid-connect-core-1_0.html)"
+                    + " standard (OAuth 2) to allow the "
+                    + "person being confirmed to log in and provide digital consent. "
+                    + "API requests are authorized using a Bearer token issued through "
+                    + "an OpenID Connect service. The token should be submitted as an "
+                    + "Authorization header in the form Bearer.\n\n"
+                    + "See our [Authorization Guide](https://developer.va.gov/explore/verification/docs/authorization)"
+                    + " for more details."
+                    + "\n\n"
+                    + "## Partial Success States"
+                    + "\n\n"
+                    + "It should be noted that partial success states can occur. "
+                    + "Partial success states occur due to:\n"
+                    + "- Address geocoding is not available\n"
+                    + "- Address geocoding is out of date compared to the users address\n"
+                    + "- Address geocoding is incomplete (e.g. missing latitude)"),
     externalDocs =
         @ExternalDocumentation(
             description = "GitHub",
