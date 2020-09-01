5190554f0551f093daf1a43dc8b8410bfcd7657a
taylorrassmannlibertyits <64798603+taylorrassmannlibertyits@users.noreply.github.com>
2020-09-01 17:24:42 -0400

    Updated to remove some swagger errors. Still need to remove GET with requestBody.

--- community-care-eligibility-api/src/main/java/gov/va/api/health/communitycareeligibility/api/CommunityCareEligibilityService.java
+++ community-care-eligibility-api/src/main/java/gov/va/api/health/communitycareeligibility/api/CommunityCareEligibilityService.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -70,10 +69,9 @@ import javax.ws.rs.Path;
 @SecurityScheme(
     type = SecuritySchemeType.OAUTH2,
     name = "OauthFlow",
-    in = SecuritySchemeIn.HEADER,
     flows =
         @OAuthFlows(
-            implicit =
+            authorizationCode =
                 @OAuthFlow(
                     authorizationUrl = "https://sandbox-api.va.gov/oauth2/authorization",
                     tokenUrl = "https://sandbox-api.va.gov/oauth2/token",
